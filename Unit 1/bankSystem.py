bbbc0cbc21e31c4472182aa9662c2bf80cc98ceb
Jmcclain7 <jmcclain865@boyslatin.net>
2026-02-25 14:32:54 +0000

    bankSystem

---
+++ Unit 1/bankSystem.py
@@ -0,0 +1,26 @@
+class JamesFirstBank:
+    def _init_ (self, name, password, address, balance):
+        self.balance = balance
+    self.name = name
+    self.password = password
+    self.adress = adress
+    self.balance = balance 
+
+    def deposit(self, balance, amount):
+        print('')
+
+    def withdraw(self, balance, amount):
+        print('')
+
+    def accessAccount(self, name, password):
+        print('')
+        # send all user info
+
+    def closeBankAccount(self, name, password):
+        print('')
+
+    def transfer(self):
+        print()
+
+account_1 = JamesFirstBank('Jim', 'Vipermoney$9', 'NA', 100)
+account_2 = JamesFirstBank('Void', 'Vipermaxmoney', 'NA', 200)
