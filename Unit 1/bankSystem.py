4b8fa73e8558263c0ab1548a66214b869c71b9c2
Jmcclain7 <jmcclain865@boyslatin.net>
2026-02-26 14:36:23 +0000

    Bank Part 2

--- Unit 1/bankSystem.py
+++ Unit 1/bankSystem.py
@@ -6,21 +6,43 @@ class JamesFirstBank:
     self.adress = adress
     self.balance = balance 
 
-    def deposit(self, balance, amount):
-        print('')
+    def deposit(self, amount):
+        self.balance += amount
+        print('thanks'+ self.name + 'your new balance is'  + str (self.balance)) 
 
-    def withdraw(self, balance, amount):
-        print('')
+    def withdraw(self,amount):
+        print('thanks' + self.name + 'your new balance is' + str(self.balance))
 
     def accessAccount(self, name, password):
         print('')
         # send all user info
 
-    def closeBankAccount(self, name, password):
-        print('')
+    def closeAccount(self, name, password):
+        return
+
+    def createAccount(self):
+        return
 
     def transfer(self):
-        print()
+        return
 
-account_1 = JamesFirstBank('Jim', 'Vipermoney$9', 'NA', 100)
+account_1 = JamesFirs+tBank('Jim', 'Vipermoney$9', 'NA', 100)
 account_2 = JamesFirstBank('Void', 'Vipermaxmoney', 'NA', 200)
+
+account_1.deposit(50)
+account_2.withdraw(30)
+
+def createAccount():
+    print('Welcome to James first bank')
+    print("please complete the following questions to create your account: ")
+    name = input("name: ")
+    password = input("password: ")
+    address= input("address: ")
+    balance= input("How much would you like to add to the new account?: ")
+
+    print("Congratulations! your bank account is done.")
+
+    account_3= JamesFirstBank(name, password, address, balance)
+    users.append(account_3)
+
+createAccount()
