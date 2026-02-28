91c7767b0169e99e4bd59be1c10fa118b04ddfdb
Jmcclain7 <jmcclain865@boyslatin.net>
2026-02-28 04:30:00 +0000

    object review

---
+++ Unit 1/objectReview.py
@@ -0,0 +1,15 @@
+"Class properties are variables that belong  to a class store information about an object created from that specific class"
+"A class method is a function inside a class that defines actions"
+
+class Student:
+    def __init__ (self, name, age, grade, gpa):
+        self. name = name
+        self.age = age
+        self.grade = grade 
+        self.gpa = gpa 
+
+    def study(self):
+        "John is studying."
+
+    def take_test(self):
+        John is taking a test
