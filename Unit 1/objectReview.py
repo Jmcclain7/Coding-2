fe938a2da776d7b3986e7d65d4ca90bd245f37e9
Jmcclain7 <jmcclain865@boyslatin.net>
2026-03-03 14:34:02 +0000

    Review

--- Unit 1/objectReview.py
+++ Unit 1/objectReview.py
@@ -1,15 +1,54 @@
+#1
 "Class properties are variables that belong  to a class store information about an object created from that specific class"
+
+#3
 "A class method is a function inside a class that defines actions"
 
+#3
 class Student:
-    def __init__ (self, name, age, grade, gpa):
-        self. name = name
+    def __init__ (self, name, age, grade, uniform):
+        self.name = name
         self.age = age
         self.grade = grade 
-        self.gpa = gpa 
+        self.uniform = uniform 
 
     def study(self):
-        "John is studying."
+        "James is studying."
 
     def take_test(self):
-        John is taking a test
+        "James is taking a test"
+
+    def is_In_Uniform(self):
+        if self.uniform == False:
+            print("this student has detention")
+        else:
+            print("student is good. no detention.")
+
+    def study(self,subject):
+        print(self.name + "is studying for" + subject)
+
+    def updateGrade(self, testScore):
+        average = 95
+        testScore += average
+        average  /= 5
+        print(average)
+
+student1 = Student(15, 10, 'John', True)
+student2 = Student(17, 11, 'Rick', False)
+
+student1.is_In_Uniform()
+student2.updateGrade(70)
+
+
+
+
+
+
+
+
+
+
+
+
+
+#4
