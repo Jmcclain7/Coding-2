eeb45db8cabfc5a931946cdbd1f189cf1c947827
Jmcclain7 <jmcclain865@boyslatin.net>
2026-03-05 14:26:45 +0000

    Libary

---
+++ Unit 2/pythonLibaries.py
@@ -0,0 +1,15 @@
+# Libary - stores books and information  - - >
+
+# Python Kibary - a file or folder that contains objects, functions, and data 
+# that we can access quickly. These collections of logic are created by other
+# programers who wanted to share their code, to make it easier for others.
+
+# Modules == Libaries *they are the same
+
+# to access a libary / module, we use the import keyword
+import random
+
+print(random,randrange(1,10))
+
+# modules/ libraies help us to speed up development time and write less code,
+# because someone already wrote it for us.
