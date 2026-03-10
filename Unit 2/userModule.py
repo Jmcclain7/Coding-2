2b354c21945e22281546271fa246971876883315
Jmcclain7 <jmcclain865@boyslatin.net>
2026-03-10 13:29:50 +0000

    activities

---
+++ Unit 2/userModule.py
@@ -0,0 +1,19 @@
+class CarModel:
+    def __init__(self, model, color, motor, horsepower):
+        self.model = model
+        self.color = color
+        self.motor = motor
+        self.horsepower = horsepower
+
+
+        def colorDetails(self):
+            print ('the car details are below:')
+            print ('car model = + 'self.model)
+            print ('car color = ' + self.color)
+
+        def model(self):
+            print('model is 2025')
+
+        def horsepower(self):
+            print("car has around 900 horsepower")
+            
